bc78f6ba117cebed2acc39296d16f85c65b7dfa6
Lorenas <lorenasbartkus@gmail.com>
2025-07-10 09:07:52 +0300

    Refactor sum functions and add comprehensive tests for SumAll and SumAllTails

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -25,5 +26,37 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d given, %v", numbers, got, want)
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of tails of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9, 3})
+		want := []int{2, 12}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9, 3})
+		want := []int{0, 12}
+
+		checkSums(t, got, want)
 	})
 }
